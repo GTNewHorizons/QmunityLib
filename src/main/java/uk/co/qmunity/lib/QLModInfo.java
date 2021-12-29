17b793f1a6462e21f3e71c0c96f4663b14f511f0
Johann Bernhardt <johann.bernhardt@tum.de>
2021-12-29 13:12:22 +0100

    Migrated to unified build script

--- src/main/java/uk/co/qmunity/lib/QLModInfo.java
+++ src/main/java/uk/co/qmunity/lib/QLModInfo.java
@@ -4,13 +4,6 @@ public class QLModInfo{
 
     public static final String MODID = "qmunitylib";
     public static final String NAME = "QmunityLib";
-    private static final String MAJOR = "@MAJOR@";
-    private static final String MINOR = "@MINOR@";
-    private static final String BUILD = "@BUILD_NUMBER@";
-    private static final String MCVERSION = "1.7.10";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
 
-    public static String fullVersionString(){
-
-        return String.format("%s-%s.%s.%s", MCVERSION, MAJOR, MINOR, BUILD);
-    }
 }
